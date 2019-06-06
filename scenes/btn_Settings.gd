b08fd6abbd00ebfc9bd8d8586b671d7b3c85df90
Cedrei <einar.ceder@gmail.com>
2019-06-06 11:07:41 +0200

    Set click mask thingies for the start screen buttons

--- scenes/btn_Settings.gd
+++ scenes/btn_Settings.gd
@@ -1,16 +1,10 @@
 extends TextureButton
 
-#var image = preload("res://assets/buttons_titlescreen/play_game.png")
-
-func _pressed():
-	# goto charcreation
-	get_tree().get_root().get_node("Root").goto_menu("char-creation")
-
 func _ready():
 	# Create click mask
 	# Load the image to a variable
 	var image = Image.new()
-	image.load("res://assets/buttons_titlescreen/play_game.png")
+	image.load("res://assets/buttons_titlescreen/settings.png")
 	# Convert image to bitmap
 	var bitmap = BitMap.new()
 	bitmap.create_from_image_alpha(image)
