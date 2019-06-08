8e7ffbfc19d2c5ce1f8d95c8f857340143de1fb5
Cedrei <einar.ceder@gmail.com>
2019-06-08 14:42:03 +0200

    Can't go from char creation screen without filling everything in

--- scripts/CharCreation.gd
+++ scripts/CharCreation.gd
@@ -23,6 +23,19 @@ func getGlobalVars(globalVar):
 	globalVar = get_tree().get_root().get_node("Root").global_vars
 	return globalVar
 
+func try_unlocking_continue_button():
+	# If all values are set to something, unlock the continue button
+	# Otherwise, lock it
+	var is_hero_name_value_set = $HeroName/HBoxContainer/HeroName.text != ""
+	var is_draco_name_value_set = $DracoName/HBoxContainer/DracoName.text != ""
+	var is_male_check_value_set = $GenderSelect/VBoxContainer/HBoxContainer/check_Male.pressed
+	var is_female_check_value_set = $GenderSelect/VBoxContainer/HBoxContainer/check_Female.pressed
+	
+	if is_hero_name_value_set and is_draco_name_value_set and (is_male_check_value_set or is_female_check_value_set):
+		$btn_continue.disabled = false
+	else:
+		$btn_continue.disabled = true
+
 #this function forces only one gender to be selected at any given time.
 func forceSingularGender(gender):
 	if gender == "male" and $GenderSelect/VBoxContainer/HBoxContainer/check_Female.pressed:
@@ -48,7 +61,17 @@ func continue_pressed():
 func _on_check_Female_toggled(button_pressed):
 	if button_pressed:
 		forceSingularGender("female")
+	try_unlocking_continue_button()
 
 func _on_check_Male_toggled(button_pressed):
 	if button_pressed:
 		forceSingularGender("male")
+	try_unlocking_continue_button()
+
+
+func _on_HeroName_text_changed():
+	try_unlocking_continue_button()
+
+
+func _on_DracoName_text_changed():
+	try_unlocking_continue_button()
